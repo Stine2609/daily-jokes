77e23af881c3fc5ddefa86354c98a40fc2b333d9
albretsen <bjellanda@gmail.com>
2024-02-20 09:31:46 +0100

    Updated useContest to use last stored value while loading

--- hooks/useContest.tsx
+++ hooks/useContest.tsx
@@ -1,20 +1,27 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect } from 'react';
 import { getContest } from "../api/contest";
-import { useIsFocused } from '@react-navigation/native'; 
+import { useIsFocused } from '@react-navigation/native';
+import { storeData, getData } from '../utils/storage'; 
 
 export const useContest = (date?: Date) => {
     const [contest, setContest] = useState({ topic: "" });
-    const isFocused = useIsFocused(); 
+    const isFocused = useIsFocused();
 
     useEffect(() => {
         let isMounted = true;
 
         const fetchTopic = async () => {
+            const storedContest = await getData('contest');
+            if (storedContest && isMounted) {
+                setContest(storedContest);
+            }
+
             if (isFocused && isMounted) {
                 try {
                     const contest_info = await getContest();
                     if (isMounted) {
                         setContest(contest_info[0]);
+                        await storeData('contest', contest_info[0]);
                     }
                 } catch (error) {
                     console.error("Failed to fetch topic:", error);
