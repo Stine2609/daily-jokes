03ad2eb114c49beb4deb1fdc51e8bb1d0aa19a80
Hallvard Hetlelid <epichallvard@hotmail.com>
2024-02-24 00:48:51 +0100

    Added FilterToggle component

---
+++ components/FilterToggle.tsx
@@ -0,0 +1,61 @@
+import { useState } from "react";
+import { View, TouchableOpacity, Image, StyleSheet } from "react-native";
+import Text from "./Text";
+import { colors } from "./Colors";
+
+interface FilterToggleProps {
+    options: Array<{
+        label: string;
+        onPress?: () => null;
+    }>
+}
+
+export default function FilterToggle(props: FilterToggleProps) {
+    const { options } = props;
+
+    // First option passed is set as active
+    const [activeFilter, setActiveFilter] = useState(0);
+    
+    return(
+        <View style={styles.container}>
+            {options.map((option, index) => 
+                <TouchableOpacity style={[styles.button, {backgroundColor: activeFilter == index ? colors.purple.highlight : colors.purple.dark}]} onPress={() => {
+                    setActiveFilter(index);
+                }}>
+                    {activeFilter == index && (
+                        <Image style={[styles.icon]} source={require("../assets/icons/check.png")} />
+                    )}
+                    <Text shadow={false}>{option.label}</Text>
+                </TouchableOpacity>
+            )}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: "row",
+        width: "80%",
+        alignSelf: "center",
+        gap: 10,
+        marginVertical: 16,
+    },
+
+    button: {
+        flexDirection: "row",
+        gap: 6,
+        backgroundColor: colors.purple.dark,
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        borderWidth: 2,
+        borderColor: colors.border,
+        borderRadius: 10,
+        minWidth: 75,
+        justifyContent: "center",
+    },
+
+    icon: {
+        height: 20,
+        width: 20,
+    }
+})
--- screens/Browse/Jokes.tsx
+++ screens/Browse/Jokes.tsx
@@ -1,10 +1,21 @@
 import { View } from "react-native";
 import SearchBar from "../../components/SearchBar";
+import FilterToggle from "../../components/FilterToggle";
 
 export default function Jokes() {
     return(
         <View>
             <SearchBar placeholder="Search..." />
+            <FilterToggle
+                options={[
+                    {
+                        label: "top",
+                    },
+                    {
+                        label: "recent",
+                    }
+                ]}
+            />
         </View>
     )
 }
