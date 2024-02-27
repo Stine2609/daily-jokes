9942023c96dcb44d210da8467dfa8b1157b9aaf5
Hallvard Hetlelid <epichallvard@hotmail.com>
2024-02-27 17:53:45 +0100

    Minor fixes to JokeListManager

--- components/managers/JokeListManager.tsx
+++ components/managers/JokeListManager.tsx
@@ -1,10 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text } from "react-native";
+import { View } from "react-native";
+import Text from '../Text';
 import JokeListItem from "../../components/listItem/JokeListItem";
 import { useJokesSearch } from "../../hooks/useJokesSearch";
+import { colors } from '../Colors';
 
-export default function JokeListManager({ criteria }) {
-    const [jokes, setJokes] = useState([]);
+interface joke {
+    user?: {
+        profile?: number;
+    };
+    userId: string;
+    textBody: string;
+    position: number;
+}
+
+interface JokeListManagerProps {
+    criteria: {};
+}
+
+export default function JokeListManager({ criteria }: JokeListManagerProps) {
+    const [jokes, setJokes] = useState<joke[]>([]);
 
     const fetchedJokes = useJokesSearch(criteria);
 
@@ -26,7 +41,7 @@ export default function JokeListManager({ criteria }) {
                     }} />
                 ))
             ) : (
-                <Text>No jokes found</Text>
+                <Text shadow={false} style={{textAlign: "center"}} color={colors.purple.dark}>You have not written any jokes yet.</Text>
             )}
         </View>
     );
