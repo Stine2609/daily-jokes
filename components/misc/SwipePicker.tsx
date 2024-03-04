519ad32aaf8d90623408322bb55bdba53aacde83
albretsen <bjellanda@gmail.com>
2024-03-04 12:59:57 +0100

    Added rating to joke swiper

--- components/misc/SwipePicker.tsx
+++ components/misc/SwipePicker.tsx
@@ -7,11 +7,13 @@ import CircularButton from '../buttons/CircularButton';
 import { componentColors, colors } from './Colors';
 import { SCREEN_HEIGHT, TAB_BAR_HEIGHT } from '../layout/ScreenView';
 import Button from '../buttons/Button';
-import { useJokesSearch } from '../../hooks/useJokesSearch';
+import { useJokesSearchSwipe } from '../../hooks/useJokesSearchSwipe';
+import { api } from '../../api/api';
+import { UserDataManager } from '../../services/userDataManager';
 
 interface CardProps {
     text: string;
-    animateCardAway: (arg0: number) => void;
+    animateCardAway: (arg0: number, arg1: string) => void;
 }
 
 function Card({ text, animateCardAway }: CardProps) {
@@ -23,15 +25,16 @@ function Card({ text, animateCardAway }: CardProps) {
                 </Text>
             </ScrollView>
             <View style={styles.buttonsContainer}>
-                <CircularButton variant="no" onPress={() => animateCardAway(-200)} />
-                <CircularButton variant="superlike" onPress={() => {}} />
-                <CircularButton variant="yes" onPress={() => animateCardAway(200)} />
+                <CircularButton variant="no" onPress={() => animateCardAway(-200, 'dislike')} />
+                <CircularButton variant="superlike" onPress={() => animateCardAway(0, 'superlike')} />
+                <CircularButton variant="yes" onPress={() => animateCardAway(200, 'like')} />
             </View>
         </ContentBox>
     )
 }
 
 interface joke {
+    id: Number;
     user?: {
         profile?: number;
         name?: string;
@@ -46,9 +49,9 @@ export default function SwipePicker() {
     const translateX = new Animated.Value(0);
 
     const [page, setPage] = useState(1);
-    const [criteria, setCriteria] = useState({ pagination: { page: page, page_size: 10 }});
+    const [criteria, setCriteria] = useState({ sortBy: "-createTimeStamp", pagination: { page: page, page_size: 10 } });
 
-    const fetchedJokes = useJokesSearch(criteria);
+    const fetchedJokes = useJokesSearchSwipe(criteria);
 
     const [jokes, setJokes] = useState<joke[]>([]);
 
@@ -126,8 +129,13 @@ export default function SwipePicker() {
     };
 
     const handleSwipe = (translationX: number) => {
+        let swipeDirection = translationX > 0 ? 'right' : 'left';
+
         if (Math.abs(translationX) > 140) {
-            animateCardAway(translationX);
+            const jokeId = jokes[currentIndex] ? jokes[currentIndex].id : null;
+            const action = swipeDirection === 'right' ? 'like' : 'dislike';
+
+            animateCardAway(translationX, action);
         } else {
             Animated.spring(translateX, {
                 toValue: 0,
@@ -136,36 +144,64 @@ export default function SwipePicker() {
         }
     };
 
-    // Animates the card if a button is pressed
-    const animateCardAway = (direction: number) => {
-        const toValue = direction > 0 ? 500 : -500;
-        Animated.timing(translateX, {
-            toValue: toValue,
-            duration: 200,
-            useNativeDriver: true,
-        }).start(() => {
-            translateX.setValue(0);
-            setCurrentIndex(currentIndex + 1);
-        });
+    const rate = async (jokeId: Number, action: string) => {
+        await api("POST", `/joke/rate/${jokeId}/${action}`, undefined, await UserDataManager.getToken());
+    }
+
+    const animateCardAway = (direction: number, action: string) => {
+        const toValue = direction !== 0 ? (direction > 0 ? 500 : -500) : 0; // For superlike, we don't move the card to the sides.
+        const jokeId = jokes[currentIndex] ? jokes[currentIndex].id : null;
+    
+        if (jokeId) {
+            rate(jokeId, action).then(() => {
+                Animated.timing(translateX, {
+                    toValue: toValue,
+                    duration: 200,
+                    useNativeDriver: true,
+                }).start(() => {
+                    translateX.setValue(0);
+                    setCurrentIndex(currentIndex + 1);
+                });
+            });
+        } else {
+            // In case there's no jokeId, just proceed with the animation.
+            Animated.timing(translateX, {
+                toValue: toValue,
+                duration: 200,
+                useNativeDriver: true,
+            }).start(() => {
+                translateX.setValue(0);
+                setCurrentIndex(currentIndex + 1);
+            });
+        }
     };
 
     const joke = "I DONT GIVE A FUCK ABOUT THE FUCKING CODE! i just want to download this stupid fucking application and use it. WHY IS THERE CODE??? MAKE A FUCKING .EXE FILE AND GIVE IT TO ME. these dumbfucks think that everyone is a developer and understands code. well i am not and i don't understand it. I only know to download and install applications. SO WHY THE FUCK IS THERE CODE? make an EXE file and give it to me. STUPID FUCKING SMELLY NERD"
 
     return (
         <View style={styles.container}>
-            <Animated.View style={[styles.nextCard, { opacity: nextCardOpacity }]}>
-                <Card text={jokes[currentIndex + 1] ? jokes[currentIndex + 1].textBody : ""} animateCardAway={animateCardAway} />
-            </Animated.View>
+            {jokes[currentIndex + 1] && (
+                <Animated.View style={[styles.nextCard, { opacity: nextCardOpacity }]}>
+                    <Card text={jokes[currentIndex + 1].textBody} animateCardAway={animateCardAway} />
+                </Animated.View>
+            )}
             <PanGestureHandler
                 onGestureEvent={onGestureEvent}
                 onHandlerStateChange={onHandlerStateChange}>
-                <Animated.View style={{
-                    transform: [{ translateX: translateX }, { rotateZ: rotateZ }],
-                    position: 'absolute',
-                    width: '100%',
-                }}>
-                    <Card text={jokes[currentIndex] ? jokes[currentIndex].textBody : ""} animateCardAway={animateCardAway} />
-                </Animated.View>
+                {jokes[currentIndex] ? (
+                    <Animated.View style={{
+                        transform: [{ translateX: translateX }, { rotateZ: rotateZ }],
+                        position: 'absolute',
+                        width: '100%',
+                    }}>
+                        <Card text={jokes[currentIndex].textBody} animateCardAway={animateCardAway} />
+                    </Animated.View>
+                ) : (
+                    <View>
+                        {/* You can customize this view as needed */}
+                        <Text>No more jokes!</Text>
+                    </View>
+                )}
             </PanGestureHandler>
         </View>
     );
